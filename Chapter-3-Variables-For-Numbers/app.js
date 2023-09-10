f9b618f9de6a985b4eac51786314b8e325cf3c1e
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-11 02:56:15 +0500

    Done!

---
+++ Chapter-3-Variables-For-Numbers/app.js
@@ -0,0 +1,20 @@
+// Chapter-3-Varibales-For-Numbers 
+
+// Question-1
+var age = 20;
+alert("I am " + age + " years old.");
+
+// Question-2
+var websiteTracker;
+websiteTracker = 15;
+alert("You have visited this site " + websiteTracker + " times.");
+
+// Question-3
+var birthYear = 2003;
+document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared varibale is number.");
+
+// Question-4
+var visitorName = "John Doe";
+var productTitle = "T-Shirt";
+var quantity = 3;
+document.write("<br>" + visitorName + " ordered " + " " + quantity + "  " + productTitle + " on XYZ store.");
