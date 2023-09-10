2a366465addd925d4aa6d5d1ce1029350c9f000a
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-11 02:17:28 +0500

    Done!

---
+++ Chapter-1-Alerts/app.js
@@ -0,0 +1,14 @@
+// Chapter-1-Alerts
+
+// Question-1
+alert("Hello!");
+
+// Question-2
+alert("Error! Please enter a valid password.");
+
+// Question-3
+alert("Welcome to JS Land... \n Happy Coding!");
+
+// Question-4
+alert("Welcome to JS Land...");
+alert("Happy Coding! \n Prevent this page for creating additional dialogs.");
