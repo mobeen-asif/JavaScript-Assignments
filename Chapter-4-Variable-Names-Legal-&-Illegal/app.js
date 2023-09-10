ae85d44f84328411a14e5a7d6723d4c4ab72881d
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-11 03:22:14 +0500

    Done!

---
+++ Chapter-4-Variable-Names-Legal-&-Illegal/app.js
@@ -0,0 +1,13 @@
+// Chapter-4-Variables-Names-Legal-&-Illegal
+
+// Question-1
+var userName = "Ali", userAge = 15, usernationality = "Pakistani";
+
+// Question-2
+// legal variables
+var carName = "Corolla", carBrand = "Toyota", modelYear = 2021, carPrice = 2500000, registered = "Karachi";
+// Illegal variables
+var visacountry = "Brazil", staydurationinyear = 2, ticketprice = 100000, dinner = "In package", tourguide = "yes";
+
+// Question-3
+document.write("Rules for naming JS varibales" + "<br>" + "Variable names can only contain numbers, $, and _. For example: $my_1stVariable" + "<br>" + "Variable must begin with a letter, $, or _. for example: $name, _name or name" + "<br>" + "variable names are case sensitive" + "<br>" + "Variable names should not be JS keywords");
