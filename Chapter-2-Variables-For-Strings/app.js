ae99d206e316074648bcd3c471442e1e42140d52
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-11 02:39:19 +0500

    Done!

---
+++ Chapter-2-Variables-For-Strings/app.js
@@ -0,0 +1,41 @@
+// Chapter-2-Variables-For-Strings
+
+// Question-1
+var userName = "Mobeen";
+
+// Question-2
+var myName = "Mobeen Asif";
+
+// Question-3
+var message;
+message = "Hello World!";
+alert(message);
+
+// Question-4
+var studentName = "Ali";
+var studentAge = "15 years old";
+var studentEducation = "Certified Mobile Application Development.";
+
+alert(studentName);
+alert(studentAge);
+alert(studentEducation);
+
+// Question-5
+var dishName = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(dishName);
+
+// Question-6
+
+var email = "mobeenasif1901@gmail.com";
+alert("My email address is " + email);
+
+// Question-7
+var book = "A smarter way to learn JavaScript.";
+alert("I am trying to learn from a book " + book );
+
+// Question-8
+document.write("Yah! I can write HTML content through JavaScript.");
+
+// Question-9
+var endingLine = "₊˚ ✧ ‿︵‿୨୧‿︵‿ ✧ ₊˚";
+alert(endingLine);
