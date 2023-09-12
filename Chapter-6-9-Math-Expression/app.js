11626655d55f6ac7a380fb702cb9cdf8aa9e4912
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-12 05:11:21 +0500

    Done!

--- Chapter-6-9-Math-Expression/app.js
+++ Chapter-6-9-Math-Expression/app.js
@@ -1,20 +1,16 @@
 // Chapter-6-9-Math-Expression 
 
 // Question-1
-var number = prompt("write any number:");
-document.write("Result:" + "<br>" + "The value of a is: " + number + "<br>" + "------------------------------");
-var incrementNumber = ++number ;
-document.write("<br>" + "The value of ++a is: " + number + "<br>" + "The new value is " + incrementNumber );
-var incrementNumber2 = incrementNumber++ ;
-document.write("<br>" + "The value of a++ is: " + incrementNumber + "<br>" + "The new value is " + incrementNumber2 );
-var decrementNumber = --incrementNumber2 ;
-document.write("<br>" + "The value of --a is: " + incrementNumber2 + "<br>" + "The new value is " + decrementNumber );
-var decrementNumber2 = decrementNumber-- ;
-document.write("<br>" + "The value of a-- is: " + decrementNumber + "<br>" + "The new value is " + decrementNumber2 );
+var a = +prompt("write any number:");
+document.write("Result:" + "<br>" + "The value of a is: " + a + "<br>" + "------------------------------" );
+document.write("<br>" + "The value of ++a is: " + ++a + "<br>" + "Now the value of a is: " + a );
+document.write("<br>" + "The value of a++ is: " + a++ + "<br>" + "Now the value of a is: " + a );
+document.write("<br>" + "The value of a++ is: " + --a + "<br>" + "Now the value of a is: " + a );
+document.write("<br>" + "The value of a++ is: " + a-- + "<br>" + "Now the value of a is: " + a );
 
 // Question-2
 var a = 2, b = 1;
-var result = --a - --b + ++b + b--;
+var result = --a - --b + ++b + b-- ;
 document.write("a = " + a + "<br>" + "b = " + b + "<br>" + "Result = " + result );
 
 // Question-3
@@ -22,23 +18,75 @@ var userName = prompt("What is your Name?");
 alert("Welcome " +  userName);
 
 // Question-4
-var num = prompt("Enter Number", "0");
+var num = prompt("Enter Number", "5");
 var num = parseInt(num);
-var i = 5;
-document.write('<table border="1" cellspacing="0">');
 for (i = 1; i <= 10; i++) {
-  document.write("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");
+  document.write( num + " x " + i + " = " + num * i + '<br>');
 }
 
 // Question-5
 var subject1 = prompt("Write name of first subject:");
 var subject2 = prompt("Write name of second subject:");
 var subject3 = prompt("Write name of third subject:");
-var totalMarks = 100;
-var marks1 = prompt("Write marks of third subject:");
-var marks2 = prompt("Write marks of third subject:");
-var marks3 = prompt("Write marks of third subject:");
-var totalMarks = 100*3;
+var totalMarksIndividual = 100 ;
+var marks1 = +prompt("Write marks of third subject:");
+var marks2 = +prompt("Write marks of third subject:");
+var marks3 = +prompt("Write marks of third subject:");
+var totalMarks = 100 * 3 ;
 var totalObtainedMarks = marks1 + marks2 + marks3 ;
-var percentage = (totalObtainedMarks/totalMarks)*100;
-document.write("<table border='1'><tr><td>Subject</td><td>Total Marks</td><td>Obtained Marks</td><td>Percentage</td></tr><tr><td>subject1</td><td>subject2</td><td>subject3</td></tr></table>")
+var percentage = ( totalObtainedMarks / totalMarks ) * 100 ;
+var subject1Percentage = ( marks1 / totalMarksIndividual ) * 100 ;
+var subject2Percentage =  ( marks2 / totalMarksIndividual ) * 100 ;
+var subject3Percentage = ( marks3 / totalMarksIndividual ) * 100 ;
+document.write(`<table>`);
+document.write(`<tr>`);
+document.write(`<td>`+ 'Subject');
+document.write(`</td>`);
+document.write(`<td>`+ 'Total Marks');
+document.write(`</td>`);
+document.write(`<td>`+ 'Obtained Marks');
+document.write(`</td>`);
+document.write(`<td>`+ 'Percentage');
+document.write(`</td>`);
+document.write(`</tr>`);
+document.write(`<tr>`);
+document.write(`<td> ${subject1} `);
+document.write(`</td>`);
+document.write(`<td> ${totalMarksIndividual} `);
+document.write(`</td>`);
+document.write(`<td> ${marks1} `);
+document.write(`</td>`);
+document.write(`<td> ${subject1Percentage} % `);
+document.write(`</td>`);
+document.write(`</tr>`);
+document.write(`<tr>`);
+document.write(`<td> ${subject2} `);
+document.write(`</td>`);
+document.write(`<td> ${totalMarksIndividual} `);
+document.write(`</td>`);
+document.write(`<td> ${marks2} `);
+document.write(`</td>`);
+document.write(`<td> ${subject2Percentage}  % `);
+document.write(`</td>`);
+document.write(`</tr>`);
+document.write(`<tr>`);
+document.write(`<td> ${subject3} `);
+document.write(`</td>`);
+document.write(`<td> ${totalMarksIndividual} `);
+document.write(`</td>`);
+document.write(`<td> ${marks3} `);
+document.write(`</td>`);
+document.write(`<td> ${subject3Percentage}  % `);
+document.write(`</td>`);
+document.write(`</tr>`);
+document.write(`<tr>`);
+document.write(`<td>`);
+document.write(`</td>`);
+document.write(`<td> ${totalMarks} `);
+document.write(`</td>`);
+document.write(`<td> ${totalObtainedMarks} `);
+document.write(`</td>`);
+document.write(`<td> ${percentage}  % `);
+document.write(`</td>`);
+document.write(`</tr>`);
+document.write(`</table>`);
---
+++ Chapter-9-11-User-Input-And-Conditional-Statement/app.js
@@ -0,0 +1,107 @@
+// Chapter-9-11-User-Input-And-Conditional-Statement 
+
+// Question-1
+// var city = prompt("Write the name of your city:");
+// if ( city === "karachi" ) {
+//     alert("Welcome to city of lights.");
+// }
+// else {
+//     alert("Invalid Input.");
+// }
+
+// Question-2
+// var gender = prompt("What is your gender?");
+// if ( gender === "male" ) {
+//     alert("Good Morning Sir");
+// }
+// else if ( gender === 'female' ) {
+//     alert("Good Morning Ma'am");
+// }
+// else {
+//     alert("Invalid Input");
+// }
+
+// Question-3
+// var signalColor = prompt("What is color of light on signal?");
+// if (signalColor === 'red') {
+//     alert('Must Stop');
+// }
+// else if(signalColor === 'yellow') {
+//     alert("Ready to Move");
+// }
+// else if (signalColor === 'green'){
+//     alert("Move now");
+// }
+// else {
+//     alert("Invalid Input");
+// }
+
+// Question-4
+// var fuelLeft = prompt("How many litres of fuel left in a car?");
+// if (fuelLeft < 0.25) {
+//     alert("Please refill the fuel in your car.");
+// }
+// else{
+//     alert("The fuel is sufficient to travel.");
+// }
+
+// Question-5
+// a 
+// var a = 4;
+// if ( ++a === 5 ) {
+//     alert("given condition for variable a is true.");
+// }
+
+// (..............Displayed.............)
+
+// b 
+// var b = 82;
+// if ( b++ === 83 ) {
+//     alert("given condition for variable b is true.");
+// }
+
+// (..............Not Displayed.............)
+
+// c 
+// var c = 12;
+// if ( c++ === 13 ) {
+//     alert("condition 1 is true.");
+// }
+// if ( c === 13 ) {
+//     alert("condition 2 is true.");
+// }
+// if ( ++c < 14 ) {
+//     alert("condition 3 is true.");
+// }
+// if ( c === 14 ) {
+//     alert("condition 4 is true.");
+// }
+
+// (..............Displayed condition 2 and 4 is true..............)
+
+// d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost ;
+// if ( totalCost === laborCost + materialCost ) {
+//     alert("The cost equals");
+// }
+
+// (..............Displayed.............)
+
+// e
+// if ( true ) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("Flase");
+// }
+
+// (..............Displayed only first condition.............)
+
+// f 
+// if ("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// (..............Displayed.............)
