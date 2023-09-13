aed7d2e47c633da1755ca0142894f397a0b1389f
Mobeen Asif <mobeenasif1901@gmail.com>
2023-09-14 01:37:30 +0500

    Done!

--- Chapter-12-13-If-Else-And-If-Else-Testing-Sets-Of-Conditions/app.js
+++ Chapter-12-13-If-Else-And-If-Else-Testing-Sets-Of-Conditions/app.js
@@ -1,8 +1,19 @@
 // Chapter-12-13-If-Else-And-Else-If-Statement-Testing-Set-Of-Conditions
 
 // Question-1
-
-
+var character = prompt("Enter a character:");
+if (!isNaN(character)) {
+    alert(character + " is a number.");
+}
+else if (character >= 'A' && character <= 'Z') {
+    alert(character + " is an uppercase letter.");
+}
+else if (character >= 'a' && character <= 'z') {
+    alert(character + " is a lowercase letter.");
+}
+else {
+    alert(character + " is not a number, uppercase letter, or lowercase letter.");
+}
 
 
 // Question-2
@@ -18,6 +29,7 @@ else {
     alert("num2 is greater than num1.")
 }
 
+
 // Question-3
 var num = +prompt("Write any number:");
 if (num > 0){
@@ -30,6 +42,7 @@ else {
     alert("Number is Negative")
 }
 
+
 // Question-4
 var word = prompt("Write any letter to check whether it is vowel or not:");
 if ( word === "a" || word === "e" || word === "i" || word === "o" || word === "u" ) {
@@ -39,15 +52,47 @@ else {
     alert("Letter is consonant.");
 }
 
+
 // Question-5
-var password = prompt("Write your password:");
-if (password !== ""){
-    alert("Please Enter your Password")
+const correctPassword = "Web & App Development";
+const userPassword = prompt("Enter your password:");
+if (!userPassword) {
+    alert("Please enter your password.");
+} else if (userPassword === correctPassword) {
+    alert("Correct! The password you entered matches the original password.");
+} else {
+    alert("Incorrect password.");
 }
-var passwordCheck = prompt("What is your password:");
-if (password === passwordCheck){
-    alert("Correct!");
+
+
+// Question-6
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
 }
 else {
-    alert("Incorrect...");
+greeting = "Good evening";
+}
+
+
+// Question-7
+var userTime = +prompt("Enter the time in 24-hour format (e.g., 1900 for 7:00 PM):");
+if (isNaN(userTime)) {
+    alert("Invalid input. Please enter a valid time in 24-hour format.");
+} 
+else if (userTime < 0 || userTime >= 2400) {
+    alert("Invalid time. Please enter a time between 0000 and 2359.");
+} 
+else if (userTime >= 0 && userTime < 1200) {
+    alert('Good morning!');
+} 
+else if (userTime >= 1200 && userTime < 1700) {
+    alert('Good afternoon!');
+} 
+else if (userTime >= 1700 && userTime < 2000) {
+    alert('Good evening!');
+} 
+else {
+    alert('Good night!');
 }
